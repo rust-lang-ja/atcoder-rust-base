c0fd088886a50ff75d865666d5ac2c3fb47d1ab3
statiolake <statiolake@gmail.com>
2019-06-28 16:01:10 +0900

    Invert order of AC and actual

--- tests/sample_inputs.rs
+++ tests/sample_inputs.rs
@@ -3,17 +3,17 @@ use procontest::*;
 #[test]
 fn sample1() {
     let res = do_test("sample1", include_str!("in1.txt"), include_str!("out1.txt"));
-    assert_eq!(res, TestResult::Accepted, "{}", format(&res));
+    assert_eq!(TestResult::Accepted, res, "{}", format(&res));
 }
 
 #[test]
 fn sample2() {
     let res = do_test("sample2", include_str!("in2.txt"), include_str!("out2.txt"));
-    assert_eq!(res, TestResult::Accepted, "{}", format(&res));
+    assert_eq!(TestResult::Accepted, res, "{}", format(&res));
 }
 
 #[test]
 fn sample3() {
     let res = do_test("sample3", include_str!("in3.txt"), include_str!("out3.txt"));
-    assert_eq!(res, TestResult::Accepted, "{}", format(&res));
+    assert_eq!(TestResult::Accepted, res, "{}", format(&res));
 }
