4b1f26627e994cde53ca007537d28225f02cf482
statiolake <statiolake@gmail.com>
2019-06-28 16:29:13 +0900

    Use testcase! macro

--- tests/sample_inputs.rs
+++ tests/sample_inputs.rs
@@ -1,19 +1,5 @@
-use procontest::*;
+use procontest::testcase;
 
-#[test]
-fn sample1() {
-    let res = do_test("sample1", include_str!("in1.txt"), include_str!("out1.txt"));
-    assert_eq!(TestResult::Accepted, res, "{}", format(&res));
-}
-
-#[test]
-fn sample2() {
-    let res = do_test("sample2", include_str!("in2.txt"), include_str!("out2.txt"));
-    assert_eq!(TestResult::Accepted, res, "{}", format(&res));
-}
-
-#[test]
-fn sample3() {
-    let res = do_test("sample3", include_str!("in3.txt"), include_str!("out3.txt"));
-    assert_eq!(TestResult::Accepted, res, "{}", format(&res));
-}
+testcase!(id: 1);
+testcase!(id: 2);
+testcase!(id: 3);
