0d59e70e3e58aa6698d743a59f988cae1111cf6f
statiolake <statiolake@gmail.com>
2019-11-16 02:20:33 +0900

    Fix test for num_derive
    There was a test file, but actually no test in the file! ;)

--- tests/test_num_derive.rs
+++ tests/test_num_derive.rs
@@ -2,22 +2,36 @@ use num_derive::{FromPrimitive, Num, NumCast, NumOps, One, ToPrimitive, Zero};
 
 #[derive(
     Debug,
-    Eq,
+    Clone,
+    Copy,
     PartialEq,
+    Eq,
     PartialOrd,
     Ord,
     FromPrimitive,
     ToPrimitive,
-    One,
     Zero,
+    One,
     Num,
     NumCast,
     NumOps,
 )]
 struct Weight(i32);
 
-fn main() {
-    let w1 = Weight(3);
+#[test]
+fn check_ops() {
+    let w1 = Weight(7);
     let w2 = Weight(4);
-    println!("{:?}", w1 + w2); // => "Weight(7)"
+
+    assert_eq!(w1 + w2, Weight(11));
+    assert_eq!(w1 - w2, Weight(3));
+    assert_eq!(w1 * w2, Weight(28));
+    assert_eq!(w1 / w2, Weight(1));
+    assert_eq!(w1 % w2, Weight(3));
+    assert!(w1 > w2);
+    assert!(w2 < w1);
+    assert!(w1 >= w1);
+    assert!(w1 <= w1);
+    assert!(w1 != w2);
+    assert!(w1 == w1);
 }
